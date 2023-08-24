b831ec9572dd192c32be971f44039d6c42288f29
Nathan Tjahjadi <nathantja@yahoo.com>
2023-08-24 17:52:32 -0500

    debugging protecting questions

--- app.py
+++ app.py
@@ -8,15 +8,14 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 
 @app.get("/")
 def survey_start():
-    """Page where user can start the survey"""
-    return render_template("survey_start.html",
-                           title=survey.title,
-                           instructions=survey.instructions)
+    """Render survey start"""
+
+    session["responses"] = []
+
+    return render_template("survey_start.html", survey=survey)
 
 
 @app.post("/begin")
@@ -25,30 +24,42 @@ def begin_survey():
     return redirect("/question/0")
 
 
+# make sure to start the survey before showing questions
+
 @app.get("/question/<int:id>")
 def display_question(id):
     """Display question"""
-    survey_question = survey.questions[id]
 
-    return render_template("question.html",
-                           question=survey_question)
+    if (session["responses"] != [] ):
+        return redirect("/")
 
+    else:
+        survey_question = survey.questions[id]
+
+        return render_template("question.html",
+                            question=survey_question)
+
+#response length could be different, can skip questions
+#have logic to fill survey in order
 
 @app.post("/answer")
 def handle_answer():
     """Appends answer value to reponses and redirects to next question or thank you"""
     answer = request.form['answer']
-    responses.append(answer)
 
-    if len(responses) == len(survey.questions):
+    responses = session["responses"]
+    responses.append(answer)
+    session["responses"] = responses
+
+    if len(session["responses"]) == len(survey.questions):
         return redirect("/thank-you")
     else:
-        return redirect(f"/question/{len(responses)}")
+        return redirect(f"/question/{len(session['responses'])}")
 
 
 @app.get("/thank-you")
 def thank_user():
     """Grabs questions and reponses and renders survey completion page"""
-    results = zip(survey.questions, responses)
+    results = zip(survey.questions, session["responses"])
 
     return render_template("completion.html", results=results)
